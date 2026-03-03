7432cc806aae7ecc6a47c5a04562bb460661b894
23122no <23122no@hvhs.school.nz>
2026-03-03 15:15:03 +1300

    collisions started

--- game.js
+++ game.js
@@ -4,12 +4,17 @@
 // Written by Nina
 /*******************************************************/
 
-let ENEMYSIZE = 50;
-let ENEMYGAP = 25;
-let ENEMYCOLOUR = "green"
-let ENEMYSPEED = 0.5;
+let ENEMYWIDTH = 60;
+let ENEMYHEIGHT = 40;
 
-let bulletShot = false;
+let VERTICALENEMYGAP = ENEMYHEIGHT;
+let HORIZONTALENEMYGAP = ENEMYWIDTH;
+
+let enemySpeed = 0.25;
+
+let PLAYERSPEED = 6;
+
+let BULLETSPEED = 8;
 
 /*******************************************************/
 // setup()
@@ -17,6 +22,9 @@ let bulletShot = false;
 function setup() {
 	console.log("setup: ");
 	cnv = new Canvas(windowWidth, windowHeight);
+
+    bulletsGroup = new Group;
+    enemyGroup = new Group;
 
     addPlayer();
     addEnemies();
@@ -29,18 +37,24 @@ function setup() {
 /*******************************************************/
 function addEnemies() {
 
-	enemyGroup = new Group;
 
     for (i = 1; i < 11; i++) {
-            for (n = 0; n < 3; n++) {
-                enemy = new Sprite(i*(ENEMYSIZE + 25), n*(ENEMYSIZE + 25), ENEMYSIZE, ENEMYSIZE, "k");
-                enemy.color = ENEMYCOLOUR;
-                enemy.bounciness = 0;
-                enemyGroup.add(enemy);
-            }
+        for (n = 0; n < 3; n++) {
+            enemy = new Sprite(
+                i*(ENEMYWIDTH + HORIZONTALENEMYGAP) + windowWidth/2 - (ENEMYWIDTH * 6 + HORIZONTALENEMYGAP * 4 + HORIZONTALENEMYGAP), 
+                n*(ENEMYWIDTH + VERTICALENEMYGAP),
+                ENEMYWIDTH, 
+                ENEMYHEIGHT, 
+                "k"
+            );
+            
+            enemy.color = "green";
+            enemyGroup.add(enemy);
+        }
     }
      
-    enemyGroup.vel.y = ENEMYSPEED;
+    enemyGroup.vel.y = enemySpeed;
+    enemyGroup.bounciness = 0;
 
     player.collides(enemyGroup, loseGame);
 
@@ -50,7 +64,7 @@ function addEnemies() {
 // addPlayer()
 /*******************************************************/
 function addPlayer() {
-	player = new Sprite(windowWidth/2, windowHeight/2 + 300, 25, "k");
+	player = new Sprite(windowWidth/2, windowHeight/2 + 300, 25, "d");
     player.color = "blue"
 }
 
@@ -66,18 +80,28 @@ function loseGame() {
 /*******************************************************/
 function fireBullet() {
     bullet = new Sprite(player.x, player.y, 10, "d");
-    bullet.bounciness = 0;
-    bullet.vel.y = -5;
-    bulletShot = true;
-    bullet.collides(enemyGroup, bulletHit);
+    bullet.vel.y = -BULLETSPEED;
+    bulletsGroup.add(bullet);
+    bulletsGroup.collides(enemyGroup, enemyHit);
+    bulletsGroup.collides(walls, wallsHit);
 }
 
 /*******************************************************/
-// bulletHit()
+// enemyHit()
 /*******************************************************/
-function bulletHit() {
-    console.log("bullet hit");
+function enemyHit(_bullet, _enemy) {
+    console.log("enemyHit");
+    _bullet.remove();
+    _enemy.remove();
 }
+
+/*******************************************************/
+// wallsHit()
+/*******************************************************/
+function wallsHit(_bullet) {
+    _bullet.remove();
+}
+
 
 /*******************************************************/
 // addWalls()
@@ -87,26 +111,25 @@ function addWalls() {
 
     walls = new Group;
 
-	wallLH  = new Sprite(0, height/2, 40, height, 'k');
-	wallLH.bounciness = 0;
+	wallLH  = new Sprite(0, height/2, 40, height, 's');
     wallLH.color = "white";
     walls.add(wallLH);
 
 
-	wallRH  = new Sprite(width, height/2, 40, height, 'k');
-	wallRH.bounciness = 0;
+	wallRH  = new Sprite(width, height/2, 40, height, 's');
     wallRH.color = "white";
     walls.add(wallRH);
 
-	wallTop  = new Sprite(width/2, 0, width, 40, 'k');
-	wallTop.bounciness = 0;
+	wallTop  = new Sprite(width/2, 0, width, 40, 's');
     wallTop.color = "white";
     walls.add(wallTop);
 
-	wallBot  = new Sprite(width/2, height, width, 40, 'k');
-	wallBot.bounciness = 0;
+	wallBot  = new Sprite(width/2, height, width, 40, 's');
     wallBot.color = "white";
     walls.add(wallBot);
+
+    walls.strokeWeight = 0;
+    walls.bounciness = 0;
 
 }
 
@@ -118,11 +141,11 @@ function draw() {
 
     if (kb.pressing('left')) {
 
-		player.velocity.x = -5;
+		player.velocity.x = -PLAYERSPEED;
 
 	} else if (kb.pressing ('right')) {
 		
-		player.velocity.x = 5;
+		player.velocity.x = PLAYERSPEED;
 	};
 
 	if (kb.released('left')) {
@@ -137,10 +160,18 @@ function draw() {
         loseGame();
     }
 
-    if (kb.pressed ("space") && bulletShot == false) {
-        console.log("space pressed");
+    if (kb.pressed ("space") && bulletsGroup.length < 3) {
         fireBullet();
     }
+
+    if (enemyGroup.length = 0) {
+        console.log("win");
+    }
+
+    player.vel.y = 0;
+    player.y = windowHeight/2 + 300
+
+    bulletsGroup.vel.x = 0;
  
 }
 
